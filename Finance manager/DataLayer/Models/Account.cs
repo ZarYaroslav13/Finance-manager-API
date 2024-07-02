ac4c4187cf0d73d6ce5ca51ff01ca7151e435380
Ярослав Суркіс <mirta13tato@gmail.com>
2024-07-02 18:42:16 +0300

    Added support for migrations

---
+++ Finance manager/DataLayer/Models/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models;
+
+public class Account : Base.Entity
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Login {  get; set; }
+
+    public string Password { get; set; }
+
+    public string Email { get; set; }
+}
