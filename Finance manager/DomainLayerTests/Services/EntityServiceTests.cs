ba84a74a1af3f6b9c5004fa7d7c29bee76b50fe4
Ярослав Суркіс <mirta13tato@gmail.com>
2024-08-06 18:52:49 +0300

    Added EntityService tests

---
+++ Finance manager/DomainLayerTests/Services/EntityServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using DataLayer.Models;
+using DataLayer.UnitOfWork;
+using DomainLayer.Services.Accounts;
+using FakeItEasy;
+
+namespace DomainLayerTests.Services;
+
+[TestClass]
+public class EntityServiceTests
+{
+    [TestMethod]
+    public void Constructor_ArgumentsAreNull_ThrowException()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => new AccountService(A.Dummy<IUnitOfWork>(), null));
+        Assert.ThrowsException<ArgumentNullException>(() => new AccountService(null, A.Dummy<IMapper>()));
+        Assert.ThrowsException<ArgumentNullException>(() => new AccountService(null, null));
+    }
+
+    [TestMethod]
+    public void Constructor_CreatedNeededReposetory_Reposetory()
+    {
+        var unitOfWork = A.Fake<IUnitOfWork>();
+
+        var service = new AccountService(unitOfWork, A.Dummy<IMapper>());
+
+        A.CallTo(() => unitOfWork.GetRepository<Account>()).MustHaveHappenedOnceExactly();
+    }
+}
