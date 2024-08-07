1f4670383c4d7570dcf58945ad51dc2c74272563
Ярослав Суркіс <mirta13tato@gmail.com>
2024-08-07 17:05:06 +0300

    Added FinanceReportDTO

---
+++ Finance manager/Finance manager API/Models/FinanceReport.cs
@@ -0,0 +1,29 @@
+﻿using DomainLayer.Models;
+
+namespace Finance_manager_API.Models;
+
+public class FinanceReportDTO : Base.ModelDTO
+{
+    public int WalletId { get; }
+    public string WalletName { get; } = String.Empty;
+    public int TotalIncome { get; }
+    public int TotalExpense { get; }
+    public List<FinanceOperationDTO> Operations { get; }
+    public Period Period { get; }
+
+    public FinanceReportDTO(
+        int walletId,
+        string walletName,
+        int totalIncome,
+        int totalExpense,
+        List<FinanceOperationDTO> operations,
+        Period period)
+    {
+        WalletId = walletId;
+        WalletName = walletName ?? throw new ArgumentNullException(nameof(walletName));
+        TotalIncome = totalIncome;
+        TotalExpense = totalExpense;
+        Operations = operations ?? throw new ArgumentNullException(nameof(operations));
+        Period = period;
+    }
+}
