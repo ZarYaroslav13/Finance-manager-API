abc2ebef49b0ce9a0e10b89f8ba34df9b6b58a72
Ярослав Суркіс <mirta13tato@gmail.com>
2024-09-04 16:43:20 +0300

    created production appsetting

--- Finance manager/ApplicationLayer/Controllers/FinanceReportController.cs
+++ Finance manager/ApplicationLayer/Controllers/FinanceReportController.cs
@@ -5,35 +5,34 @@ using DomainLayer.Services.Finances;
 using DomainLayer.Services.Wallets;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ApplicationLayer.Controllers
+namespace ApplicationLayer.Controllers;
+
+public class FinanceReportController : BaseController
 {
-    public class FinanceReportController : BaseController
+    private readonly IFinanceReportCreator _creator;
+    private readonly IWalletService _walletService;
+
+    public FinanceReportController(IFinanceReportCreator financeReportCreator, IWalletService walletService, IMapper mapper, ILogger<BaseController> logger) : base(mapper, logger)
     {
-        private readonly IFinanceReportCreator _creator;
-        private readonly IWalletService _walletService;
+        _creator = financeReportCreator ?? throw new ArgumentNullException(nameof(financeReportCreator));
+        _walletService = walletService ?? throw new ArgumentNullException(nameof(walletService));
+    }
 
-        public FinanceReportController(IFinanceReportCreator financeReportCreator, IWalletService walletService, IMapper mapper, ILogger<BaseController> logger) : base(mapper, logger)
-        {
-            _creator = financeReportCreator ?? throw new ArgumentNullException(nameof(financeReportCreator));
-            _walletService = walletService ?? throw new ArgumentNullException(nameof(walletService));
-        }
+    [HttpPost("CreateDaily")]
+    public async Task<FinanceReportDTO> CreateReportAsync(int walletId, DateTime date)
+    {
+        var wallet = await _walletService.FindWalletAsync(walletId);
 
-        [HttpPost("CreateDaily")]
-        public async Task<FinanceReportDTO> CreateReportAsync(int walletId, DateTime date)
-        {
-            var wallet = await _walletService.FindWalletAsync(walletId);
+        return _mapper.Map<FinanceReportDTO>(
+                await _creator.CreateFinanceReportAsync(wallet, date));
+    }
 
-            return _mapper.Map<FinanceReportDTO>(
-                    await _creator.CreateFinanceReportAsync(wallet, date));
-        }
+    [HttpPost("CreatePeriodReport")]
+    public async Task<FinanceReportDTO> CreateReportAsync(int walletId, DateTime startDate, DateTime endDate)
+    {
+        var wallet = await _walletService.FindWalletAsync(walletId);
 
-        [HttpPost("CreatePeriodReport")]
-        public async Task<FinanceReportDTO> CreateReportAsync(int walletId, DateTime startDate, DateTime endDate)
-        {
-            var wallet = await _walletService.FindWalletAsync(walletId);
-
-            return _mapper.Map<FinanceReportDTO>(
-                    await _creator.CreateFinanceReportAsync(wallet, startDate, endDate));
-        }
+        return _mapper.Map<FinanceReportDTO>(
+                await _creator.CreateFinanceReportAsync(wallet, startDate, endDate));
     }
 }
