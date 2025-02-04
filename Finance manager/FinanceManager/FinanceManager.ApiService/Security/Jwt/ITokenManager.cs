d6a6c0d8f00c072db77a69c1ffae73be06073f46
Yark13 <yaroslav.zarytskyi.ki.2021@lpnu.ua>
2025-02-04 14:46:51 +0200

    Added Security to FinanceManagerApi

---
+++ Finance manager/FinanceManager/FinanceManager.ApiService/Security/Jwt/ITokenManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace FinanceManager.Web.Security.Jwt;
+
+public interface ITokenManager
+{
+    public string CreateToken(ClaimsIdentity identity);
+
+    public Task<ClaimsIdentity> GetAccountIdentityAsync(string email, string password);
+
+    public Task<ClaimsIdentity> GetAdminIdentityAsync(string email, string password);
+}
